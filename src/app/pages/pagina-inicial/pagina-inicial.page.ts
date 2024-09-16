710af0b922717c84cefa14291b6e643cfccf26c8
Gabriela Madalosso <gbmadalosso@gmail.com>
2024-09-15 23:29:00 -0300

    Criada Página Inicial

---
+++ src/app/pages/pagina-inicial/pagina-inicial.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-pagina-inicial',
+  templateUrl: './pagina-inicial.page.html',
+  styleUrls: ['./pagina-inicial.page.scss'],
+})
+export class PaginaInicialPage {
+
+  constructor(private router: Router, private http: HttpClient) {}
+
+  logout() {
+    this.http.post('http://localhost:5001/logout', {}).subscribe({
+      next: () => {
+        this.router.navigate(['/']); 
+      },
+      error: (error) => {
+        console.error('Falha no logout', error);
+      }
+    });
+  }
+}
